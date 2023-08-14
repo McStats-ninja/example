202b88767e0568eed7640059784cba9daaefce8e
Freddi <mail@FrederikHeinrich.de>
2023-08-15 01:31:02 +0200

    Create ExampleStandaloneNinja.java

---
+++ src/main/java/org/example/standalone/ExampleStandaloneNinja.java
@@ -0,0 +1,13 @@
+package org.example.standalone;
+
+import ninja.mcstats.api.McStatsNinja;
+import ninja.mcstats.api.Ninja;
+
+public class ExampleStandaloneNinja {
+
+    public static void main(String[] args) {
+        Ninja ninja = McStatsNinja.init("demo#standalone");
+    }
+
+
+}

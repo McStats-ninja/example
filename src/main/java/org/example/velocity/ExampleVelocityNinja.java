24ef37b6fc5d2fd0aa4ca6c677678b4504f1ff43
Freddi <mail@FrederikHeinrich.de>
2023-08-15 01:31:13 +0200

    Update ExampleVelocityNinja.java

--- src/main/java/org/example/velocity/ExampleVelocityNinja.java
+++ src/main/java/org/example/velocity/ExampleVelocityNinja.java
@@ -3,6 +3,8 @@ package org.example.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import ninja.mcstats.api.McStatsNinja;
+import ninja.mcstats.api.Ninja;
 
 @Plugin(id = "joinme", name = "JoinMe", version = "@version@", description = "JoinMe plugin for Velocity", authors = {"Freddi"}, url = "https://joinme.freddi.io")
 public class ExampleVelocityNinja {
@@ -11,6 +13,9 @@ public class ExampleVelocityNinja {
     @Inject
     public void onProxyInitialize(ProxyInitializeEvent event) {
         System.out.println("Hello, world!");
+        Ninja ninja = McStatsNinja.init("demo#velocity");
+
+        
     }
 
 }

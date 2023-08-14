5581121d33e58b2eabf09f8f8ab3fe5707371f54
Freddi <mail@FrederikHeinrich.de>
2023-08-15 01:31:09 +0200

    Update ExampleBungeeNinja.java

--- src/main/java/org/example/bungee/ExampleBungeeNinja.java
+++ src/main/java/org/example/bungee/ExampleBungeeNinja.java
@@ -1,12 +1,17 @@
 package org.example.bungee;
 
 import net.md_5.bungee.api.plugin.Plugin;
+import ninja.mcstats.api.McStatsNinja;
+import ninja.mcstats.api.Ninja;
 
 public class ExampleBungeeNinja extends Plugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Hello, world!");
+
+        Ninja ninja = McStatsNinja.init("demo#bungee");
+        
     }
-    
+
 }

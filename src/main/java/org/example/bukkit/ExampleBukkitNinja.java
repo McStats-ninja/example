6772fed6ae7caf5d064676707339e109fdf938f4
Freddi <mail@FrederikHeinrich.de>
2023-08-21 22:17:58 +0200

    Update ExampleBukkitNinja.java

--- src/main/java/org/example/bukkit/ExampleBukkitNinja.java
+++ src/main/java/org/example/bukkit/ExampleBukkitNinja.java
@@ -2,7 +2,6 @@ package org.example.bukkit;
 
 import ninja.mcstats.api.McStatsNinja;
 import ninja.mcstats.api.Ninja;
-import ninja.mcstats.api.exception.UnknownShurikenKeyException;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExampleBukkitNinja extends JavaPlugin {
@@ -12,12 +11,6 @@ public class ExampleBukkitNinja extends JavaPlugin {
         getLogger().info("Hello, world!");
 
         Ninja ninja = McStatsNinja.init("demo#bukkit");
-
-        try {
-            ninja.addShuriken("test", new Ninja.Shuriken<Object>(() -> "test"));
-        } catch (UnknownShurikenKeyException e) {
-            throw new RuntimeException(e);
-        }
 
     }
 }
